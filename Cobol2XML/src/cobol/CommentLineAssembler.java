087e98db3741371d0b6847233ab92501818ef212
MichaelJarratt <mike.jarratt@virginmedia.com>
2020-05-02 19:31:37 +0100

    COMMENT LINES - changed CommentLineAssembler so that the whole comment is converted to XML, it's a botch job, will need revisiting but the output is correct.

--- Cobol2XML/src/cobol/CommentLineAssembler.java
+++ Cobol2XML/src/cobol/CommentLineAssembler.java
@@ -15,13 +15,23 @@ public class CommentLineAssembler extends Assembler
 	@Override
 	public void workOn(Assembly a)
 	{
-		Cobol c = new Cobol(); //represents line of code
+		Cobol c = new Cobol(); // represents line of code
 		Token t = (Token) a.pop(); //represents a single word in the line
+		
+		
 		if(t.sval() != null) //if token has a string value
 		{
-			c.setCommentLine(t.sval().trim());
+			c.setCommentLine(t.sval()+" "+a.remainder(" "));
 			a.setTarget(c);
 		}
+//		StringBuffer buff = new StringBuffer();
+//		while (((Token) a.peek()).sval() != null) // while there are remaining tokens with comment words in them
+//		{
+//			buff.append(((Token) a.pop()).sval()+" "); //appends token and a space to String Buffer
+//		}
+//		String commentLine = buff.toString(); //converts buffer to string
+//		c.setCommentLine(commentLine.trim()); //removes extra space and sets it as the comment line for the Cobol Object
+//		a.setTarget(c);
 	}
 
 }
