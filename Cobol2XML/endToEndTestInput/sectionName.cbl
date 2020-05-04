19ba4765c7eff700949ec81f4dfcf3a0adc20ce5
MichaelJarratt <mike.jarratt@virginmedia.com>
2020-05-04 21:37:06 +0100

    END TO END TESTING - adding end to end acceptance tests for sectionName, dateWritten, commentLine.

---
+++ Cobol2XML/endToEndTestInput/sectionName.cbl
@@ -0,0 +1,1 @@
+working-storage section.
--- Cobol2XML/src/testing/EndToEndTests.java
+++ Cobol2XML/src/testing/EndToEndTests.java
@@ -15,19 +15,95 @@ public class EndToEndTests
 {
 
 	@Test
-	public void testProgramID() throws Exception
+	public void testProgramID()
 	{
 		String[] args = new String[2];
-		args[0] = "endToEndTestInput/base.cbl"; //input file
+		args[0] = "endToEndTestInput/programID.cbl"; //input file
 		args[1] = "endToEndTestInput/output.xml"; //output file
-		//Cobol2XML cobol2Xml = new Cobol2XML(args);
-		Cobol2XML.main(args);
 		
-		InputStream is = new FileInputStream(args[1]); //reads from output file
-		BufferedReader r = 	new BufferedReader(new InputStreamReader(is));
-		r.readLine();
-		r.readLine(); //skips two lines because they will always be generated regardless
-		assertTrue(r.readLine().equals("<Program-ID>base_jb</Program-ID>")); //asserts that the program ID line is parsed correctly
+		try
+		{
+			Cobol2XML.main(args);
+			InputStream is = new FileInputStream(args[1]); //reads from output file
+			BufferedReader r = 	new BufferedReader(new InputStreamReader(is));
+			r.readLine();
+			r.readLine(); //skips two lines because they will always be generated regardless
+			assertTrue(r.readLine().trim().equals("<Program-ID>base_jb</Program-ID>")); //asserts that the program ID line is parsed correctly
+			r.close();
+		} catch (Exception e)
+		{
+			fail("Cobol2XML threw an Exception");
+		}
+		
 	}
 
+	@Test
+	public void testSectionName()
+	{
+		String[] args = new String[2];
+		args[0] = "endToEndTestInput/sectionName.cbl"; //input file
+		args[1] = "endToEndTestInput/output.xml"; //output file
+		
+		try
+		{
+			Cobol2XML.main(args);
+			InputStream is = new FileInputStream(args[1]); //reads from output file
+			BufferedReader r = 	new BufferedReader(new InputStreamReader(is));
+			r.readLine();
+			r.readLine(); //skips two lines because they will always be generated regardless
+			assertTrue(r.readLine().trim().equals("<section>working-storage</section>")); //asserts that the working storage line is parsed correctly	
+			r.close();
+		} 
+		catch (Exception e)
+		{
+			fail("Cobol2XML threw an Exception");
+		}	
+	}
+	
+	@Test
+	public void testDateWritten()
+	{
+		String[] args = new String[2];
+		args[0] = "endToEndTestInput/dateWritten.cbl"; //input file
+		args[1] = "endToEndTestInput/output.xml"; //output file
+		
+		try
+		{
+			Cobol2XML.main(args);
+			InputStream is = new FileInputStream(args[1]); //reads from output file
+			BufferedReader r = 	new BufferedReader(new InputStreamReader(is));
+			r.readLine();
+			r.readLine(); //skips two lines because they will always be generated regardless
+			assertTrue(r.readLine().trim().equals("<day-date-written>7</day-date-written>")); //asserts that the date written line is parsed correctly	
+			assertTrue(r.readLine().trim().equals("<month-date-written>jul</month-date-written>"));
+			assertTrue(r.readLine().trim().equals("<year-date-written>1995</year-date-written>"));
+			r.close();
+		} 
+		catch (Exception e)
+		{
+			fail("Cobol2XML threw an Exception");
+		}
+	}
+	
+	@Test public void testCommentLine()
+	{
+		String[] args = new String[2];
+		args[0] = "endToEndTestInput/commentLine.cbl"; //input file
+		args[1] = "endToEndTestInput/output.xml"; //output file
+		
+		try
+		{
+			Cobol2XML.main(args);
+			InputStream is = new FileInputStream(args[1]); //reads from output file
+			BufferedReader r = 	new BufferedReader(new InputStreamReader(is));
+			r.readLine();
+			r.readLine(); //skips two lines because they will always be generated regardless
+			assertTrue(r.readLine().trim().equals("<comment>convert from decimal to base system</comment>")); //asserts that the comment line is parsed correctly	
+			r.close();
+		} 
+		catch (Exception e)
+		{
+			fail("Cobol2XML threw an Exception");
+		}
+	}
 }
