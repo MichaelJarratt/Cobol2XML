73083a6f11eeb1e3008cb2c32a02335a879607c2
MichaelJarratt <mike.jarratt@virginmedia.com>
2020-05-07 01:46:08 +0100

    removed depricated files

--- Cobol2XML/src/AllTests.java
+++
@@ -1,9 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-@RunWith(Suite.class)
-@SuiteClasses({ ConstantValueTest.class, DivisionTests.class, SectionStorageTest.class, CobolTest.class })
-public class AllTests {
-
-}
